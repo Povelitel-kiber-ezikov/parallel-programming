f5e6c2bc7886bcc0697990968d91766beb25bf02
Povelitel-kiber-ezikov <mr.timur1705@gmail.com>
2026-02-11 13:15:38 +0400

    добавлена функция перемножения матриц

--- lab1/mat.cxx
+++ lab1/mat.cxx
@@ -29,10 +29,34 @@ void read_mat(const std::string& filepath, std::vector<std::vector<float>>& mat)
     file.close();
 }
 
+//перемножение матриц
+std::vector<std::vector<float>> mult_mat(const std::vector<std::vector<float>>& A, const std::vector<std::vector<float>>& B){
+    if(A.empty() || B.empty() || A[0].size() != B.size()){
+        std::cerr << "Неверные размеры" << std::endl;
+        return;
+    }
+
+    std::vector<std::vector<float>> C;
+    
+    for(size_t i = 0; i < A.size(); ++i){
+        for(size_t j = 0; j < B[0].size(); ++j){
+            for (size_t k = 0; k < B.size(); ++k){
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    
+    return C;
+    
+
+}
+
 
 
 int main(){
     std::vector<std::vector<float>> A;
     std::vector<std::vector<float>> B;
+    
+
     return 0;
 }
